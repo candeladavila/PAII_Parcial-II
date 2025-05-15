e5dab967ead020482ca907c2445ef87f21d46de2
candeladavila <candeladavila05@gmail.com>
2025-05-15 12:17:21 +0200

    Septiembre_2021

--- src/main/scala/2021/Septiembre 2021/Examen Resuelto/DatoCompartido.scala
+++ src/main/scala/2021/Septiembre 2021/Examen Resuelto/DatoCompartido.scala
@@ -6,18 +6,20 @@ class DatoCompartido(private val nProcesadores: Int) {
   //VARIABLES INICIALES
   // Dato a procesar
   private var dato: Int = 0
-  // Número de procesadores totales = nProcesadores (parámetro)
-  // Número de procesadores pendientes de procesar el dato
-  private var procPend: Int = 0
-
+  // Número de procesadores pendientes
+  private var nProc = 0
   /**
    * Recibe como parámetro el número de procesadores que tienen que manipular
    * cada dato generado. Debe ser un número mayor que 0.
    */
 
   //VARIABLES
-  private val mutex_dato = new Semaphore(1) //mutex sobre la variable dato
-  private val mutex_proc = new Semaphore(0)
+  private val mutex_dato = new Semaphore(1) //sobre la variable dato
+  private val mutex_proc = new Semaphore(1) //sobre la variable procPend
+  private val procesadores = Array.fill(10)(new Semaphore(0)) //creamos un array de tamaño 10 con los semáforos de los 10 procesadores
+  private val genera = new Semaphore(1) //inicialmente se puede generar
+  private val finProcesamiento = new Semaphore(0) //inicialmente no se ha terminado ningún procesamiento
+  private val leer = new Semaphore(0) //inicialmente nadie puede leer
 
   /**
    * El Generador utiliza este método para almacenar un nuevo dato a procesar.
@@ -31,14 +33,31 @@ class DatoCompartido(private val nProcesadores: Int) {
    */
 
   def generaDato(d: Int): Int = {
+    genera.acquire()
+    mutex_dato.acquire()
+    mutex_proc.acquire()
+    dato = d //nuevo dato generado
     println(s"Dato a procesar: $dato")
-    println(s"Número de procesadores pendientes: $procPend")
-    0
+    nProc = nProcesadores //reiniciamos el número de procesadores pendientes
+    println(s"Número de procesadores pendientes: $nProc")
+    //PROCESAMOS
+    mutex_dato.release()
+    mutex_proc.release()
+    leer.release()
+    procesadores.foreach(_.release()) //liberamos a todos los procesadores
+    //FIN PROCESAMIENTO
+    finProcesamiento.acquire()
+    mutex_dato.acquire()
+    var datoFinal = dato
+    mutex_dato.release()
+    genera.release()
+    datoFinal
   }
 
   /**
    * El Procesador con identificador id utiliza este método para leer el
-   * dato que debe procesar (el dato se devuelve como valor de retorno del método).
+   * dato que debe procesar (el dato se devuelve como valor de re
+   * torno del método).
    * Deberá esperarse si no hay datos nuevos para procesar
    * o si otro procesador está manipulando el dato.
    *
@@ -46,7 +65,12 @@ class DatoCompartido(private val nProcesadores: Int) {
    * CS2_Procesador: espera a que el dato esté disponible para procesarlo.
    */
   def leeDato(id: Int): Int = {
-    0
+    procesadores(id).acquire()
+    leer.acquire()
+    mutex_dato.acquire()
+    var datoActual = dato
+    mutex_dato.release()
+    datoActual
   }
 
   /**
@@ -56,6 +80,20 @@ class DatoCompartido(private val nProcesadores: Int) {
    * (2) Si él era el último procesador avisará al Generador de que han terminado.
    */
   def actualizaDato(id: Int, datoActualizado: Int): Unit = {
-
+    mutex_dato.acquire()
+    mutex_proc.acquire()
+    dato = datoActualizado
+    println(s"\tProcesador $id ha procesado el dato. Nuevo dato: $dato")
+    nProc -=1 //restamos 1 a los procesadores pendientes
+    println(s"Numero de procesadores pendientes: $nProc")
+    if (nProc > 0){ //quedan procesadores pendientes
+      mutex_dato.release()
+      mutex_proc.release()
+      leer.release()
+    }else{ //ya han procesado todos los procesadores
+      mutex_dato.release()
+      mutex_proc.release()
+      finProcesamiento.release() //liberamos al fin del generador
+    }
   }
 }
