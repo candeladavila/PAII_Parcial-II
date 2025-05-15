8d8fd6e654cf8c204c4942a2c728ab8eea8ca1fb
candeladavila <candeladavila05@gmail.com>
2025-05-15 17:19:33 +0200

    Septiembre 2020

--- src/main/scala/2020/Septiembre 2020/Examen Resuelto/Curso.scala
+++ src/main/scala/2020/Septiembre 2020/Examen Resuelto/Curso.scala
@@ -8,46 +8,42 @@ class Curso {
   private val ALUMNOS_AV = 3
 
   //VARIABLES
-  private var alumnosIniciacion = 0 //inicialmente no hay alumnos
-  private var alumnosGrupo = 0 //inicialmente no hay alumnos
-  private val mutex1 = new Semaphore(1) //acesso a la variable alumnosIniciacion
-  private val mutex2 = new Semaphore(1) //acceso a la variable ALUMNOS_AV
-  private val inicioIniciacion = new Semaphore(1) //inicialmente se puede entrar a iniciación
-  private val finIniciacion = new Semaphore(0) //inicialmente nadie termina iniciación //SOBRA
-  private val esperaAvanzada = new Semaphore(0) //inicialmente nadie empieza avanzada
-  private val esperaGrupo = new Semaphore(1) //inicialmente no hay ningún grupo
-  private val inicioProyecto = new Semaphore(0) //inicialmente no hay ningún proyecto empezado en avanzada
-  private val finProyecto = new Semaphore(0) //inicialmente nadie termina avanzada
+  private var alumnos_ini = 0
+  private var esperando_grupo = 0
+  private var terminado_proyecto = 0
+
+  private val mutex = new Semaphore(1)
+  private val inicio_ini = new Semaphore(1) //inicialmente se puede acceder al curso de iniciación (quedan plazas)
+  private val inicio_ava = new Semaphore(0) //inicialmente nadie puede acceder a avanzada
+  private val espera_grupo = new Semaphore(1)
+  private val espera_fin_proyecto = new Semaphore(0)
+
 
   // El alumno tendrá que esperar si ya hay 10 alumnos cursando la parte de iniciación
   @throws[InterruptedException]
   def esperaPlazaIniciacion(id: Int): Unit = {
-    // Espera si ya hay 10 alumnos cursando esta parte
-    inicioIniciacion.acquire()
-    mutex1.acquire()
-    alumnosIniciacion +=1
+    inicio_ini.acquire()
+    mutex.acquire()
+    alumnos_ini += 1
     // Mensaje a mostrar cuando el alumno pueda conectarse y cursar la parte de iniciación
     println(s"PARTE INICIACION: Alumno $id cursa parte iniciacion")
-    finIniciacion.release() //dejamos que finalice
-    if (alumnosIniciacion < MAX_ALUMNOS_INI){ //si quedan plazas libres deja al resto entrar
-      inicioIniciacion.release()
-    } //si no quedan plazas libres entonces no libera nada
-    mutex1.release()
+    if (alumnos_ini < MAX_ALUMNOS_INI){ //quedan plazas libres -> deja abierta la puerta
+      inicio_ini.release()
+    }
+    mutex.release()
   }
 
   // El alumno informa que ya ha terminado de cursar la parte de iniciación
   @throws[InterruptedException]
   def finIniciacion(id: Int): Unit = {
-    finIniciacion.acquire()
-    mutex1.acquire()
-    alumnosIniciacion -= 1 //termina iniciación y deja su plaza libre
+    mutex.acquire()
+    alumnos_ini -= 1
     // Mensaje a mostrar para indicar que el alumno ha terminado la parte de principiantes
     println(s"PARTE INICIACION: Alumno $id termina parte iniciacion")
-    // Libera la conexión pAlumno.scalaara que otro alumno pueda usarla
-    inicioIniciacion.release()
-    mutex1.release()
-    // Inicia el proceso de espera para la plaza de avanzada
-    esperaAvanzada.release() //empieza a esperar
+    if (alumnos_ini == MAX_ALUMNOS_INI-1){ //es el primero en salir, abre la puerta para que pueda salir otro
+      inicio_ini.release()
+    }
+    mutex.release()
   }
 
   /* El alumno tendrá que esperar:
@@ -56,23 +52,27 @@ class Curso {
    */
   @throws[InterruptedException]
   def esperaPlazaAvanzado(id: Int): Unit = {
-    // Espera a que no haya otro grupo realizando esta parte
-    esperaAvanzada.acquire()
-    mutex2.acquire()
-    esperaGrupo.acquire() //primero tiene que ver si hay alguien que ya esté dentro de avanzada o no
-    // Espera a que haya tres alumnos conectados
-    alumnosGrupo += 1 //se suma al grupo
+    espera_grupo.acquire() //espera a que lleguen los que falten del grupo para empezar
+    mutex.acquire()
+    esperando_grupo += 1
     // Mensaje a mostrar si el alumno tiene que esperar al resto de miembros en el grupo
     println(s"PARTE AVANZADA: Alumno $id espera a que haya $ALUMNOS_AV alumnos")
-    if (alumnosGrupo < ALUMNOS_AV){ //faltan componentes para empezar el grupo
-      mutex2.release()
-      esperaGrupo.release() //deja que entren más alumnos
-    } else{ //ya están todos los componentes -> empieza el proyecto
+    if (esperando_grupo < ALUMNOS_AV){ //si todavía no son suficientes para empezar el proyecto
+      //dejamos al siguiente alumno entrar
+      espera_grupo.release()
+      //bloqueo para esperar a los otros dos compañeros
+      mutex.release()
+      espera_fin_proyecto.acquire() //se bloquea aquí para esperar al resto
+      mutex.acquire() //vuelvo a coger el mutex cuando me he desbloqueado
+    } else{ //esperando_grupo == ALUMNOS_AV
       // Mensaje a mostrar cuando el alumno pueda empezar a cursar la parte avanzada
-      println(s"PARTE AVANZADA: Hay $ALUMNOS_AV alumnos. Alumno $id empieza el proyecto")
-      mutex2.release()
-      inicioProyecto.release()
+      println(s"PARTE AVANZADA: Hay $esperando_grupo alumnos. Alumno $id empieza el proyecto")
     }
+    esperando_grupo -= 1
+    if (esperando_grupo != 0){
+      espera_fin_proyecto.release() //desbloqueamos a los tres alumnos
+    }
+    mutex.release()
   }
 
   /* El alumno:
@@ -82,18 +82,22 @@ class Curso {
   @throws[InterruptedException]
   def finAvanzado(id: Int): Unit = {
     // Espera a que los 3 alumnos terminen su parte avanzada
-    inicioProyecto.acquire()
-    mutex2.acquire()
-    alumnosGrupo -= 1 //actualiza el índice de personas pendientes por terminar
+    mutex.acquire()
+    terminado_proyecto += 1 //termina su parte del proyecto
     // Mensaje a mostrar si el alumno tiene que esperar a que los otros miembros del grupo terminen
     println(s"PARTE AVANZADA: Alumno $id termina su parte del proyecto. Espera al resto")
-    if (alumnosGrupo > 0){ //quedan compañeros por terminar -> vuelve a abrir la puerta de inicioProyecto
-      inicioProyecto.release()
-    } else{ //es el último en terminar
-      // Mensaje a mostrar cuando los tres alumnos del grupo han terminado su parte
-      println(s"PARTE AVANZADA: LOS $ALUMNOS_AV ALUMNOS HAN TERMINADO EL CURSO")
-      esperaGrupo.release() //nuevo grupo
+    if (terminado_proyecto < ALUMNOS_AV){ //si aún no han terminado todos
+      mutex.release()
+      espera_fin_proyecto.acquire() //se bloquea aquí hasta que termine el resto
+    } else { //si ya han terminado todos
+      println(s"PARTE AVANZADA: LOS $terminado_proyecto ALUMNOS HAN TERMINADO EL CURSO") //aquí alummnos_ava == ALUMNOS_AV
     }
-    mutex2.release()
+    terminado_proyecto -= 1 //termina
+    if (terminado_proyecto != 0){ //desbloqueamos en cascada
+      espera_fin_proyecto.release()
+    } else {
+      espera_grupo.release()
+    }
+    mutex.release()
   }
 }
