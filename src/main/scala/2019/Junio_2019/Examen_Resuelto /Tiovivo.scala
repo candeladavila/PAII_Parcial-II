3bbbce21ce9bc4ed7e9333de9e80c74eafcdc6ac
candeladavila <candeladavila05@gmail.com>
2025-05-15 17:37:43 +0200

    Junio 2019

--- src/main/scala/2019/Junio_2019/Examen_Resuelto /Tiovivo.scala
+++ src/main/scala/2019/Junio_2019/Examen_Resuelto /Tiovivo.scala
@@ -1,18 +1,47 @@
+import java.util.concurrent._
+
 class Tiovivo(capacidad: Int) {
+  //VARIABLES
+  private var nPasajeros = 0
+  private val mutex = new Semaphore(1)
+  private val puertaEntrada = new Semaphore(1) //inicialmente el tiovivo está vacio y se puede entrar
+  private val puertaSalida = new Semaphore(0) //inicialmente la puerta de salida está cerrada
+  private val lleno = new Semaphore(0) //inicialmente está vacio, se libera cuando se llene
 
   def subir(id: Int): Unit = {
-    // TODO
+    puertaEntrada.acquire()
+    mutex.acquire()
+    nPasajeros += 1
+    println(s"El pasajero $id se sube. Quedan ${capacidad-nPasajeros} plazas.")
+    if (nPasajeros < capacidad){ //si quedan plazas libres
+      puertaEntrada.release()
+    } else{
+      lleno.release()
+    }
+    mutex.release()
   }
 
   def bajar(id: Int): Unit = {
-    // TODO
+    puertaSalida.acquire()
+    mutex.acquire()
+    nPasajeros -= 1
+    println(s"El pasajero $id se baja. Quedan ${nPasajeros}.")
+    if (nPasajeros > 0){
+      puertaSalida.release()
+    } else{
+      println("************************************************")
+      puertaEntrada.release()
+    }
+    mutex.release()
   }
 
   def esperaLleno(): Unit = {
-    // TODO
+    lleno.acquire()
+    println("TIOVIVO LLENO, EMPIEZA EL VIAJE!!!")
   }
 
   def finViaje(): Unit = {
-    // TODO
+    println("FIN DEL VIAJE :(")
+    puertaSalida.release()
   }
 }
